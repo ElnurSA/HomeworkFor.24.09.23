e96112992d2896ab07721b6f53ca82f5a31e49a2
ElnurSA <elnurms@code.edu.az>
2023-09-23 18:10:32 +0400

    first lines added

---
+++ During-Lesson/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+static string ReverseStr(string str)
+{
+    string result = string.Empty;
+    for (var i = str.Length - 1; i >= 0; i--)
+    {
+        result += str[i];
+    }
+
+    return result;
+
+}
+
+Console.WriteLine(ReverseStr("Semed"));
